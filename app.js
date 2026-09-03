05381040119fbd9bfcd1340269c93aa9d66536b3
Asgerzadeh <ilkinasgerzade49@gmail.com>
2026-09-03 18:27:52 +0400

    Task 1

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
 // Task 1
-// let elementH1 = document.querySelector("h1")
-// elementH1.remove();
-
-// let body = document.querySelector("body")
-// let script = document.querySelector("script")
-
-// let ListText = document.querySelector("li")
-// ListText.textContent = "RTX3000";
-// body.insertBefore(ListText, script)
-// console.log(ListText);
-
-
-// Task 2
 let elementH1 = document.querySelector("h1")
-elementH1.innerText = "Series"
-console.log(elementH1);
+elementH1.remove();
+
+let body = document.querySelector("body")
+let script = document.querySelector("script")
+
+let ListText = document.querySelector("li")
+ListText.textContent = "RTX3000";
+body.insertBefore(ListText, script)
+console.log(ListText);
 
 
-let ClassPrimary = document.querySelector(".primary")
- ClassPrimary.style.background = "teal"
+
 
 
 
