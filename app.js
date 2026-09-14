d29d65b558112ed36414940f4a389b4e2ccf081d
Asgerzadeh <ilkinasgerzade49@gmail.com>
2026-09-14 18:18:54 +0400

    Task 1 14.09

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+let form = document.querySelector("form");
 
-document.addEventListener("keyup", (e)=> {
-    if (e.key == "Enter")
-        console.log('ENTER');
 
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let myForm = new FormData(e.target)
+
+//User Age ucun olan hisse
+    console.log(myForm.get("user-age"));
+
+//Mail ucun olan hisse
+    if (!myForm.get("user-email").includes("@"))
+        console.log("Mail yazilis yanlisdir!");
+    else
+        console.log(myForm.get("user-email"));
+
+//Password ucun olan hisse
+    if (myForm.get("user-password").length > 5)
+        console.log("Giris Icaze Verildi!");
+    else 
+        console.log("Giris Qadagandir!");
 })
+
 
 
 
